2d4594cf44740d0e9d16c8096d4e6dc904541a38
emir battal <emir.battalbas@solidict.com>
2020-10-14 23:38:07 +0300

    initial controller added

--- MarketApp/App/AppDelegate.swift
+++ MarketApp/App/AppDelegate.swift
@@ -1,21 +1,21 @@
-//
-//  AppDelegate.swift
-//  MarketApp
-//
-//  Created by emirhan battalbaş on 14.10.2020.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+  
+  var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    
+    self.window = UIWindow()
+    self.window?.backgroundColor = .clear
+    self.window?.makeKeyAndVisible()
+    let navigationController = UINavigationController(rootViewController: MainViewController())
+//    navigationController.isNavigationBarHidden = true
+    self.window?.rootViewController = navigationController
+    
     return true
   }
-
+  
 }
 
