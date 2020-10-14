0a81e093df05129cdaaf6995e89bf353285e294d
emir battal <emir.battalbas@solidict.com>
2020-10-14 23:19:27 +0300

    project created and first adjustments made

---
+++ MarketApp/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MarketApp
+//
+//  Created by emirhan battalbaş on 14.10.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
